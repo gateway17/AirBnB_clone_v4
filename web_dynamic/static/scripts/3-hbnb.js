fc9a046d7f626c1fa15ac6b6e3cfd8b499a5e447
vanemcb <vanem2201@gmail.com>
2021-09-26 12:46:07 -0500

    Trying do a POST request

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,23 @@
+$('input[type=checkbox]').css('margin-right', '10px');
+
+$('input[type=checkbox]').change(function () {
+  const amtyobj = {};
+  $.each($('input[type=checkbox]:checked'), function () {
+    amtyobj[$(this).attr('data-name')] = ($(this).attr('data-id'));
+  });
+  const amntyList = Object.keys(amtyobj);
+  $('.amenities h4').text(amntyList);
+});
+
+$.get('http://172.22.150.35:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+});
+
+const placesDict = {};
+$.post('http://172.22.150.35:5001/api/v1/places_search', placesDict, function (data, status) {
+  alert(status)
+});
